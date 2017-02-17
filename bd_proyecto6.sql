a950dc5614da53466500465f19b8c0dbdc1edac8
edhuchacaliaza <93191.joan23@fje.edu>
2017-02-17 16:07:29 +0100

    Base de datos actualizada

--- bd_proyecto6.sql
+++ bd_proyecto6.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-02-2017 a las 15:12:43
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 7.0.6
+-- Tiempo de generación: 17-02-2017 a las 16:06:24
+-- Versión del servidor: 10.1.10-MariaDB
+-- Versión de PHP: 5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bd_proyecto6`
 --
+CREATE DATABASE IF NOT EXISTS `bd_proyecto6` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bd_proyecto6`;
 
 -- --------------------------------------------------------
 
@@ -28,29 +30,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tbl_alumno` (
   `alu_id` int(5) NOT NULL,
-  `alu_nom` varchar(15) COLLATE utf16_spanish2_ci NOT NULL,
-  `alu_apellido` varchar(50) COLLATE utf16_spanish2_ci NOT NULL,
+  `alu_nom` varchar(15) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `alu_apellido` varchar(50) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
   `alu_nfpublico` double(4,2) NOT NULL,
   `alu_nftribunal` double(4,2) NOT NULL,
   `proy_id` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish2_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tbl_notasalumno`
---
-
-CREATE TABLE `tbl_notasalumno` (
-  `na_id` int(5) NOT NULL,
-  `na_nombre` varchar(30) COLLATE utf16_spanish2_ci NOT NULL,
-  `na_curso` varchar(50) COLLATE utf16_spanish2_ci NOT NULL,
-  `na_MAC` varchar(30) COLLATE utf16_spanish2_ci NOT NULL,
-  `na_nota` int(2) NOT NULL,
-  `preg_id` int(2) NOT NULL,
-  `alu_id` int(5) NOT NULL,
-  `proy_id` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish2_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -61,12 +46,29 @@ CREATE TABLE `tbl_notasalumno` (
 CREATE TABLE `tbl_notasprofe` (
   `np_id` int(5) NOT NULL,
   `np_nota` int(2) NOT NULL,
-  `np_comentario` text COLLATE utf16_spanish2_ci NOT NULL,
+  `np_comentario` text CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
   `preg_id` int(2) NOT NULL,
   `alu_id` int(5) NOT NULL,
-  `profe_id` int(3) NOT NULL,
-  `proy_id` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish2_ci;
+  `pro_id` int(3) NOT NULL,
+  `np_fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_notaspublico`
+--
+
+CREATE TABLE `tbl_notaspublico` (
+  `na_id` int(5) NOT NULL,
+  `na_nombre` varchar(30) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `na_curso` varchar(50) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `na_MAC` varchar(30) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `na_nota` int(2) NOT NULL,
+  `preg_id` int(2) NOT NULL,
+  `alu_id` int(5) NOT NULL,
+  `na_fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -76,10 +78,10 @@ CREATE TABLE `tbl_notasprofe` (
 
 CREATE TABLE `tbl_pregunta` (
   `preg_id` int(2) NOT NULL,
-  `preg_txt` text COLLATE utf16_spanish2_ci NOT NULL,
-  `preg_tipo` enum('Presentación Oral','Contenido') COLLATE utf16_spanish2_ci NOT NULL,
-  `preg_categoria` enum('Profesor','Alumno') COLLATE utf16_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish2_ci;
+  `preg_txt` text CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `preg_tipo` enum('Presentación Oral','Contenido') CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `preg_categoria` enum('Profesor','Alumno') CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -89,12 +91,12 @@ CREATE TABLE `tbl_pregunta` (
 
 CREATE TABLE `tbl_profesor` (
   `pro_id` int(3) NOT NULL,
-  `pro_nombre` varchar(15) COLLATE utf16_spanish2_ci NOT NULL,
-  `pro_apellido` varchar(50) COLLATE utf16_spanish2_ci NOT NULL,
-  `pro_usuario` varchar(15) COLLATE utf16_spanish2_ci NOT NULL,
-  `pro_pass` varchar(15) COLLATE utf16_spanish2_ci NOT NULL,
-  `pro_tipo` enum('admin','jurado') COLLATE utf16_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish2_ci;
+  `pro_nombre` varchar(15) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `pro_apellido` varchar(50) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `pro_usuario` varchar(15) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `pro_pass` varchar(15) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `pro_tipo` enum('admin','profesor','jurado') CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -104,13 +106,13 @@ CREATE TABLE `tbl_profesor` (
 
 CREATE TABLE `tbl_proyecto` (
   `proy_id` int(5) NOT NULL,
-  `proy_nom` varchar(100) COLLATE utf16_spanish2_ci NOT NULL,
-  `proy_curso` varchar(50) COLLATE utf16_spanish2_ci NOT NULL,
-  `proy_ano` varchar(20) COLLATE utf16_spanish2_ci NOT NULL,
+  `proy_nom` varchar(100) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `proy_curso` varchar(50) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
+  `proy_ano` varchar(20) CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
   `proy_pin` int(4) NOT NULL,
-  `proy_estado` enum('abierto','cerrado') COLLATE utf16_spanish2_ci NOT NULL,
+  `proy_estado` enum('abierto','cerrado') CHARACTER SET utf16 COLLATE utf16_spanish2_ci NOT NULL,
   `proy_nota` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish2_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Índices para tablas volcadas
@@ -123,16 +125,16 @@ ALTER TABLE `tbl_alumno`
   ADD PRIMARY KEY (`alu_id`);
 
 --
--- Indices de la tabla `tbl_notasalumno`
---
-ALTER TABLE `tbl_notasalumno`
-  ADD PRIMARY KEY (`na_id`);
-
---
 -- Indices de la tabla `tbl_notasprofe`
 --
 ALTER TABLE `tbl_notasprofe`
   ADD PRIMARY KEY (`np_id`);
+
+--
+-- Indices de la tabla `tbl_notaspublico`
+--
+ALTER TABLE `tbl_notaspublico`
+  ADD PRIMARY KEY (`na_id`);
 
 --
 -- Indices de la tabla `tbl_pregunta`
@@ -162,15 +164,15 @@ ALTER TABLE `tbl_proyecto`
 ALTER TABLE `tbl_alumno`
   MODIFY `alu_id` int(5) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT de la tabla `tbl_notasalumno`
---
-ALTER TABLE `tbl_notasalumno`
-  MODIFY `na_id` int(5) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT de la tabla `tbl_notasprofe`
 --
 ALTER TABLE `tbl_notasprofe`
   MODIFY `np_id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tbl_notaspublico`
+--
+ALTER TABLE `tbl_notaspublico`
+  MODIFY `na_id` int(5) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `tbl_pregunta`
 --
