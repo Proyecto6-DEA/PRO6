88678e52ce895791a3a3a362e4691f00698c0b9c
danvabe <danvabe@hotmail.com>
2017-02-21 15:47:38 +0100

    ajax

---
+++ proyecto/tribunal.php
@@ -0,0 +1,58 @@
+<?php
+
+
+
+
+  ?>
+
+ 
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- PROPIEDADES META WEB -->
+        <meta http-equiv="Content-Type" content="text/html">
+        <meta name="description" content="Gestio de Grups no Academics">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- TITULO DE LA PAGINA -->
+        <title>Gestio Grups 2.5</title>
+        <!-- FAVICON DE LA PAGINA -->
+        <link rel="icon" type="image/png" href="img/favicon.png" />
+        <!-- JQUERY -->
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <!-- BOOTSTRAP -->
+        <link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <!-- BOOTSTRAP TABLE -->
+        <link rel="stylesheet" href="css/bootstrap-table.css">
+        <script type="text/javascript" src="js/bootstrap-table.js"></script>
+        <script type="text/javascript" src="js/bootstrap-table-ca-ES.js"></script>
+        <!-- BOOTSTRAP TABLE COOKIE -->
+        <script type="text/javascript" src="js/bootstrap-table-cookie.js"></script>
+        <!-- BOOTSTRAP TABLE EXPORT -->
+        <script type="text/javascript" src="js/bootstrap-table-export.js"></script>
+        <script type="text/javascript" src="js/tableExport.min.js"></script>
+        <script src="//oss.maxcdn.com/bootbox/4.2.0/bootbox.min.js"></script>
+        <!-- FONT AWESOME -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+        
+        <!-- LIBRO DE ESTILOS CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        
+        <div class="page-header ">
+            CONTENIDO DEL HEADER
+            
+        </div>
+
+        <div class="container">
+            <div class="col-md-12">
+                <div role="tabpanel" class="row">
+                    <table id="tblTribunalGestion" data-height="610" data-cookie="true" data-cookie-id-table="uno"  data-toolbar="#toolbar2"></table>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript" src="tablaJs/tbl_tribunal_gestion.js"></script>
+    </body>
+</html>
